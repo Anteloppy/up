402e8be25faa6934a9964e11951fdbcaf4fb3e53
Anteloppy <polsonya04@gmail.com>
2023-11-25 12:32:08 +0700

    1.2

--- add_wins/FinancialTransaction_add.xaml.cs
+++ add_wins/FinancialTransaction_add.xaml.cs
@@ -29,13 +29,13 @@ namespace up.add_wins
         private static string connectionString = "server=localhost; port=3306; database=accounting_finance; user=root; password=Nimda123;";
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            string add = "insert into audit_logs(id, datetime, user, action, description) values(" + Convert.ToInt32(TBaudit_log_id.Text) + ", '" + Convert.ToDateTime(TBdatetime.Text) + "', " + Convert.ToInt32(TBuser.Text) + ", '" + TBaction.Text + "', '" + TBdescription.Text + "); commit;";
+            string add = "insert into financial_transactions(id, date, type, amount, sender, receiver) values(" + Convert.ToInt32(TBfinancial_transaction_id.Text) + ", '" + DateOnly.FromDateTime(Convert.ToDateTime(TBdate.Text)) + "', '" + TBtype.Text + "', " + Convert.ToDecimal(TBamount.Text) + ", '" + TBsender.Text + "', '" + TBreceiver.Text + "' ); commit;";
             MySqlConnection conn = new MySqlConnection(connectionString);
             conn.Open();
             MySqlCommand cmd = new MySqlCommand(add, conn);
             cmd.ExecuteNonQuery();
             conn.Close();
-            MessageBox.Show("аудит добавлен");
+            MessageBox.Show("финансовая операция добавлена");
         }
     }
 }
--- add_wins/IncomeExpense_add.xaml.cs
+++ add_wins/IncomeExpense_add.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,17 @@ namespace up.add_wins
         public IncomeExpense_add()
         {
             InitializeComponent();
+        }
+        private static string connectionString = "server=localhost; port=3306; database=accounting_finance; user=root; password=Nimda123;";
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string add = "insert into income_expenses(id, date, type, category, amount) values(" + Convert.ToInt32(TBincome_expense_id.Text) + ", '" + DateOnly.FromDateTime(Convert.ToDateTime(TBdate.Text)) + "', '" + TBtype.Text + "', '" + TBcategory.Text + "', " + Convert.ToDecimal(TBamount.Text) + " ); commit;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(add, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("финансовая операция добавлена");
         }
     }
 }
